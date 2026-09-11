0ba4466dc36e0fea2ddc729a08d08745b9529993
Dinar <dinaraznaev370@gmail.com>
2026-09-11 20:53:02 +0300

    add home_1

---
+++ Lab_1/Home_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите строку: ");
+string str = Console.ReadLine().ToLower();
+
+Console.Write("Введите символ: ");
+string input = Console.ReadLine();
+char charr = char.ToLower(input[0]); 
+
+int count = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i] == charr)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Символ '{charr}' встречается в строке {count} раз(а).");
