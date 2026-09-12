a801ee8a79fdb915c139641df30f1aa5037c27af
Dinar <dinaraznaev370@gmail.com>
2026-09-12 15:34:11 +0300

    add lab_1

---
+++ Lab_1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите число от 1 до 7: ");
+int d = int.Parse(Console.ReadLine());
+
+string N;
+
+switch (d)
+{
+    case 1: N = "Понедельник"; break;
+    case 2: N = "Вторник"; break;
+    case 3: N = "Среда"; break;
+    case 4: N = "Четверг"; break;
+    case 5: N = "Пятница"; break;
+    case 6: N = "Суббота"; break;
+    case 7: N = "Воскресенье"; break;
+    default: N = "Некорректное число"; break;
+}
+
+Console.WriteLine(N);
