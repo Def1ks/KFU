2b0d96404967ea64728b1598f4c75a0445d8bfe6
Dinar <dinaraznaev370@gmail.com>
2026-09-12 16:35:30 +0300

    add task xzzz kakoy

--- Labs/Lab_1/Program.cs
+++ Labs/Lab_1/Program.cs
@@ -30,31 +30,80 @@
 //     Console.WriteLine("Год не високосный");
 // }
 
-Console.Write("Введите первое число: ");
-int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine());
 
-Console.Write("Меню: \n1 - Сложить \n2 - Вычесть \n3 - Умножить \n4 - Разделить\n");
-int num = int.Parse(Console.ReadLine());
+// Console.Write("Меню: \n1 - Сложить \n2 - Вычесть \n3 - Умножить \n4 - Разделить\n");
+// int num = int.Parse(Console.ReadLine());
 
-int sum = 0;
+// int sum = 0;
 
-switch (num)
-{
-    case 1: Console.WriteLine(sum = a + b); break;
-    case 2: Console.WriteLine(sum = a - b); break;
-    case 3: Console.WriteLine(sum = a * b); break;
-    case 4: 
-    if (b != 0) 
-        {
-            Console.WriteLine(sum = a / b);
-        }
-        else
-        {
-            Console.WriteLine("Деление на ноль");
-        }
-        ; break;
-    default: Console.WriteLine("Ошибка"); break;
-}
+// switch (num)
+// {
+//     case 1: Console.WriteLine(sum = a + b); break;
+//     case 2: Console.WriteLine(sum = a - b); break;
+//     case 3: Console.WriteLine(sum = a * b); break;
+//     case 4: 
+//     if (b != 0) 
+//         {
+//         Console.WriteLine(sum = a / b);
+//         }
+//         else
+//         {
+//             Console.WriteLine("Деление на ноль");
+//         }
+//         ; break;
+//     default: Console.WriteLine("Ошибка"); break;
+// }
+
+Console.Write("Введите коэффициент a: ");
+    if (!double.TryParse(Console.ReadLine(), out double a))
+    {
+        Console.WriteLine("Ошибка ввода a.");
+        return;
+    }
+
+Console.Write("Введите коэффициент b: ");
+    if (!double.TryParse(Console.ReadLine(), out double b))
+    {
+        Console.WriteLine("Ошибка ввода b.");
+        return;
+    }
+
+Console.Write("Введите коэффициент c: ");
+    if (!double.TryParse(Console.ReadLine(), out double c))
+    {
+        Console.WriteLine("Ошибка ввода c.");
+        return;
+    }
+
+    if (a == 0)
+    {
+        Console.WriteLine("Коэффициент 'a' не может быть равен 0 (это не квадратное уравнение).");
+        return;
+    }
+
+double D = b * b - 4 * a * c;
+Console.WriteLine($"Дискриминант D = {D}");
+
+    if (D > 0)
+    {
+        double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+        Console.WriteLine($"Уравнение имеет два корня:");
+        Console.WriteLine($"x1 = {x1}");
+        Console.WriteLine($"x2 = {x2}");
+    }
+    else if (D == 0)
+    {
+        double x = -b / (2 * a);
+        Console.WriteLine($"Уравнение имеет один корень:");
+        Console.WriteLine($"x = {x}");
+    }
+    else 
+    {
+        Console.WriteLine("Действительных корней нет (D < 0).");
+    }
--- Labs/Lab_1/obj/Debug/net8.0/lab_1.AssemblyInfo.cs
+++ Labs/Lab_1/obj/Debug/net8.0/lab_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3603e75fa2ed7eaca669246b08065ac30d2e8ed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f834fb14f6fdd70366a001566095873086acfca")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
